31ea1d3ad5a19d0af5bd013f011eeb9620fe9693
Mingli Yuan <mingli.yuan@gmail.com>
2014-03-05 11:54:45 +0800

    try to support pipeline

--- src/java/com/guokr/hebo/server/ServerCallback.java
+++ src/java/com/guokr/hebo/server/ServerCallback.java
@@ -1,25 +1,32 @@
 package com.guokr.hebo.server;
 
+import java.nio.ByteBuffer;
+
 import com.guokr.hebo.HeboCallback;
 
 public class ServerCallback extends HeboCallback {
 
-	public RespCallback response;
+	public int cur;
 	public int pipesize;
+	public ByteBuffer[] buffers;
+	public RespCallback response;
 
 	public ServerCallback(int size, RespCallback cb) {
 		this.response = cb;
+		this.cur = 0;
 		this.pipesize = size;
+		this.buffers = new ByteBuffer[size];
 	}
 
 	public void response() {
 		if (this.buffer == null) {
 			this.error("Unknown server error!");
 		}
+		buffers[cur++] = this.buffer;
 
 		if (this.pipesize == 1) {
 			this.buffer.flip();
-			this.response.run(this.buffer);
+			this.response.run(this.buffers);
 		} else {
 			this.pipesize--;
 		}
